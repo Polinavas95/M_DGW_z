eb060e90b2998f286dc1947423f62bca0e1684b8
Кафедра 24 <dep24cpp@yandex.ru>
2018-04-28 09:05:29 +0300

    Update PrimaryGen.cc

--- src/PrimaryGen.cc
+++ src/PrimaryGen.cc
@@ -9,10 +9,6 @@ PrimaryGen::~PrimaryGen()
 { }
 
 void PrimaryGen::GeneratePrimaries(G4Event* anEvent) {
-    gun->SetParticleDefinition(G4Gamma::GammaDefinition());
-    gun->SetParticleEnergy(0.661 * MeV);
-    gun->SetParticlePosition(G4ThreeVector(0 * mm, 0 * mm, 0 * mm));
-    gun->SetParticleMomentumDirection(
-            G4ThreeVector(2 * G4UniformRand() - 1, 2 * G4UniformRand() - 1, 2 * G4UniformRand() - 1));
+
     gun->GeneratePrimaryVertex(anEvent);
 }
